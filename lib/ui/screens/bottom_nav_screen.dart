17fbd749253e0aff5b9ad01d14e0333d6cbd4131
Saon00 <sikdersaon1@gmail.com>
2023-07-30 00:28:47 +0600

    bottom navbad added

---
+++ lib/ui/screens/bottom_nav_screen.dart
@@ -0,0 +1,52 @@
+import 'package:demo/ui/screens/cart_screen.dart';
+import 'package:demo/ui/screens/categories_screen.dart';
+import 'package:demo/ui/screens/home_screen.dart';
+import 'package:demo/ui/screens/wishlist_screeen.dart';
+import 'package:demo/ui/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavScreen extends StatefulWidget {
+  const BottomNavScreen({super.key});
+
+  @override
+  State<BottomNavScreen> createState() => _BottomNavScreenState();
+}
+
+class _BottomNavScreenState extends State<BottomNavScreen> {
+  final List<Widget> _screen = [
+    const HomeScreen(),
+    const CategoriesScreen(),
+    const CartScreen(),
+    const WishListScreen(),
+  ];
+
+  int _selectIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screen[_selectIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: primaryColor,
+        unselectedItemColor: greyColor,
+        showUnselectedLabels: true,
+        currentIndex: _selectIndex,
+        onTap: (value) {
+          _selectIndex = value;
+          if (mounted) {
+            setState(() {});
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.grid_view_outlined), label: 'Categories'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart_checkout_outlined), label: 'Cart'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard), label: 'WishList'),
+        ],
+      ),
+    );
+  }
+}
