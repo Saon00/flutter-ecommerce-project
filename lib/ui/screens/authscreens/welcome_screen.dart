020c2a041fad3282284da572adcb58428555d034
Saon00 <sikdersaon1@gmail.com>
2023-09-24 16:17:25 +0600

    email verification api integrated

--- lib/ui/screens/authscreens/welcome_screen.dart
+++ lib/ui/screens/authscreens/welcome_screen.dart
@@ -16,6 +16,7 @@ class WelcomeBackScreen extends StatefulWidget {
 
 class _WelcomeBackScreenState extends State<WelcomeBackScreen> {
   final TextEditingController _emailETController = TextEditingController();
+  final GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,68 +24,78 @@ class _WelcomeBackScreenState extends State<WelcomeBackScreen> {
         return Padding(
           padding: const EdgeInsets.all(24),
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // logo
-                Image.asset(
-                  logoimgPath,
-                  width: 80,
-                  height: 80,
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                // text
-                Text(
-                  'Welcome Back',
-                  style: GoogleFonts.ubuntu(fontSize: 30),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Please enter your Email address',
-                  style: GoogleFonts.ubuntu(color: greyColor),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
+            child: Form(
+              key: _globalKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // logo
+                  Image.asset(
+                    logoimgPath,
+                    width: 80,
+                    height: 80,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  // text
+                  Text(
+                    'Welcome Back',
+                    style: GoogleFonts.ubuntu(fontSize: 30),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Please enter your Email address',
+                    style: GoogleFonts.ubuntu(color: greyColor),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
 
-                // text field
-                CommonTextField(
-                  hintText: 'Email Address ',
-                  textEditingController: _emailETController,
-                  validator: (String? value) {
-                    
-                  },
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                // button
+                  // text field
+                  CommonTextField(
+                    hintText: 'Email Address ',
+                    textEditingController: _emailETController,
+                    validator: (String? value) {
+                      if (value?.isEmpty ?? true) {
+                        return 'Enter a valid email';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  // button
 
-                authController.emailVerificationProgress
-                    ? const CircularProgressIndicator()
-                    : SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            final bool response = await authController
-                                .emailVerification(_emailETController.text);
-                            if (response) {
-                              Get.to(() => const OTPscreen());
-                            } else {
-                              if (mounted) {
-                                Get.snackbar('Login Failed', 'Try again',
-                                    backgroundColor: Colors.red);
+                  authController.emailVerificationProgress
+                      ? const CircularProgressIndicator()
+                      : SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              // validate email controller
+                              if (_globalKey.currentState!.validate()) {
+                                // email verification check
+                                final bool response = await authController
+                                    .emailVerification(_emailETController.text);
+                                if (response) {
+                                  Get.to(() => const OTPscreen());
+                                } else {
+                                  if (mounted) {
+                                    Get.snackbar('Login Failed', 'Try again',
+                                        backgroundColor: Colors.red);
+                                  }
+                                }
                               }
-                            }
-                          },
-                          child: const Text('Next'),
+                            },
+                            child: const Text('Next'),
+                          ),
                         ),
-                      ),
-              ],
+                ],
+              ),
             ),
           ),
         );
